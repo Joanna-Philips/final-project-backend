dcd1c7c8e542322b2730bfc3835645b71cbc45d1
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-06-04 23:01:54 +0200

    Create completeAdventure.js

---
+++ src/api/adventures/completeAdventure.js
@@ -0,0 +1,7 @@
+import express from "express";
+import adventureDb from "./adventureDb";
+import authenticateUser from "../users/authenticateUser";
+
+const router = express.Router();
+
+export default router;
